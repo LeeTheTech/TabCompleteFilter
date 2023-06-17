b820822adca660772ac028f1671fa2ad3cfc1e70
Lee <leecook808@gmail.com>
2023-06-17 13:03:18 -0700

    Fix

--- src/main/java/lee/code/tcf/bungee/listeners/FilterListener.java
+++ src/main/java/lee/code/tcf/bungee/listeners/FilterListener.java
@@ -2,7 +2,6 @@ package lee.code.tcf.bungee.listeners;
 
 import io.github.waterfallmc.waterfall.event.ProxyDefineCommandsEvent;
 import lee.code.tcf.bungee.TabCompleteFilter;
-import lombok.Getter;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.plugin.Command;
 import net.md_5.bungee.api.plugin.Listener;
@@ -12,8 +11,7 @@ import net.md_5.bungee.event.EventPriority;
 import java.util.*;
 
 public class FilterListener implements Listener {
-    @Getter private final TabCompleteFilter plugin;
-
+    private final TabCompleteFilter plugin;
     public FilterListener(TabCompleteFilter plugin) {
         this.plugin = plugin;
     }
@@ -22,7 +20,7 @@ public class FilterListener implements Listener {
     public void onTabCompleteResponse(ProxyDefineCommandsEvent e) {
         if (e.getReceiver() instanceof ProxiedPlayer proxiedPlayer) {
             if (proxiedPlayer.getGroups().contains("admin")) return;
-            final Set<String> whitelist = new HashSet<>(getPlugin().getConfigManager().getWhiteList());
+            final Set<String> whitelist = new HashSet<>(plugin.getConfigManager().getWhiteList());
             final Map<String, Command> filteredCommands = new HashMap<>();
 
             for (Command command : e.getCommands().values()) {
